7e257f159e67d928afc2c42e2c5f27257ade311f
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-08-29 16:08:44 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -9,6 +9,7 @@ docker-compose restart app
 docker-compose exec -T app php artisan config:cache
 yes |docker-compose exec -T app php artisan migrate
 docker-compose exec -T app php artisan passport:install
+docker-compose exec -T app php artisan instance:actor
 docker-compose restart app
 
 echo -e "admin\nadmin\n${ADMIN_EMAIL}\n${ADMIN_PASSWORD}\n${ADMIN_PASSWORD}\ny\ny\ny" | docker-compose exec -T app php artisan user:create
