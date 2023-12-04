1eb4603f73b8cd1b041627041b7531db7f493973
Prem chavhan <pchavhan@logiquad.com>
2023-12-04 15:36:16 +0530

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cp -rf ./contrib/docker/Dockerfile.apache
+cp ./contrib/docker/Dockerfile.apache ./
 mv Dockerfile.apache Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/pixelfed:latest | docker load
