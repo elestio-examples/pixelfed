1183cdabd5994dec3ba9296df6353859c4a6598d
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-02-25 17:23:47 +0200

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cp ./contrib/docker/Dockerfile.apache ./
 mv Dockerfile.apache Dockerfile
+rm -rf ./.dockerignore
 sed -i "s~libwebp7~libwebp6~g" ./Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/pixelfed:latest | docker load
--- tests/run.sh
+++ tests/run.sh
@@ -1,11 +1,4 @@
-#!/usr/bin/env bash
-set -o allexport; source .env; set +o allexport;
-docker-compose up -d;
-sleep 30s;
-
-yes |docker-compose exec -T app php artisan migrate
-
-set -o allexport; source .env; set +o allexport;
+sleep 60s;
 
 docker-compose exec -T app php artisan key:generate
 docker-compose restart app
@@ -15,8 +8,4 @@ docker-compose exec -T app php artisan passport:install --force
 docker-compose exec -T app php artisan instance:actor
 docker-compose restart app
 
-echo -e "admin\nadmin\n${ADMIN_EMAIL}\n${ADMIN_PASSWORD}\n${ADMIN_PASSWORD}\ny\ny\ny" | docker-compose exec -T app php artisan user:create
-
-sleep 30s;
-docker-compose down;
-docker-compose up -d;
+sleep 60s;
