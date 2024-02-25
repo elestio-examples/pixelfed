0ea53742b11d645257227b94a2096cbe4ef3d9cc
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-02-25 16:46:06 +0200

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cp ./contrib/docker/Dockerfile.apache ./
 mv Dockerfile.apache Dockerfile
+sed -i "s~libwebp7~libwebp6~g" ./Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/pixelfed:latest | docker load
