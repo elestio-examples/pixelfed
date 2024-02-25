4417706f66db4c630aff3cda594b0e6d8e17602e
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-02-25 17:46:21 +0200

    fix

--- tests/run.sh
+++ tests/run.sh
@@ -1,11 +1,14 @@
 sleep 60s;
 
 docker-compose exec -T app php artisan key:generate
-docker-compose restart app
+# docker-compose restart app
 docker-compose exec -T app php artisan config:cache --force
 docker-compose exec -T app php artisan migrate --force
 docker-compose exec -T app php artisan passport:install --force
 docker-compose exec -T app php artisan instance:actor
-docker-compose restart app
+# docker-compose restart app
+
+docker-compose down
+docker-compose up -d;
 
 sleep 60s;
