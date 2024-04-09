1afba53a46ed08c97b0881e28826824a67fe533b
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-04-09 11:29:06 +0300

    fix

--- tests/run.sh
+++ tests/run.sh
@@ -1,17 +1,17 @@
 docker-compose up -d;
 sleep 60s;
 docker-compose exec -T app php artisan key:generate
-docker-compose down;
-docker-compose up -d;
+# docker-compose down;
+# docker-compose up -d;
+docker-compose restart app
 sleep 60s;
-# docker-compose restart app
 docker-compose exec -T app php artisan config:cache --force
 docker-compose exec -T app php artisan migrate --force
 docker-compose exec -T app php artisan passport:install --force
 docker-compose exec -T app php artisan instance:actor
-# docker-compose restart app
+docker-compose restart app
 
-docker-compose down
-docker-compose up -d;
+# docker-compose down
+# docker-compose up -d;
 
 sleep 60s;
