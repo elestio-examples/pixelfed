5e0c18973ccba158b63b94a84fd7b99026cd245c
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-05-22 19:43:39 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-cp ./contrib/docker/Dockerfile.apache ./
-mv Dockerfile.apache Dockerfile
+
+sed -i 's~PHP_VERSION="8.1"~PHP_VERSION="8.3"~g' ./Dockerfile
+sed -i 's~PHP_BASE_TYPE="apache"~PHP_BASE_TYPE="fpm"~g' ./Dockerfile
+
 rm -rf ./.dockerignore
 sed -i "s~libwebp7~libwebp6~g" ./Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/pixelfed:latest | docker load
--- tests/run.sh
+++ tests/run.sh
@@ -1,11 +1,11 @@
 docker-compose up -d;
 sleep 60s;
-docker-compose exec -T app php artisan key:generate
-docker-compose down;
-docker-compose up -d;
-sleep 60s;
+# docker-compose exec -T app php artisan key:generate
+# docker-compose down;
+# docker-compose up -d;
+# sleep 60s;
 # docker-compose restart app
-docker-compose exec -T app php artisan config:cache --force
+docker-compose exec -T app php artisan config:cache 
 docker-compose exec -T app php artisan migrate --force
 docker-compose exec -T app php artisan passport:install --force
 docker-compose exec -T app php artisan instance:actor
