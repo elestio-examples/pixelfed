1c120ab3f00a143f1fc78bcc65baa275cf868fbb
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-07-20 22:10:27 +0300

    fix

--- tests/run.sh
+++ tests/run.sh
@@ -12,7 +12,7 @@ docker-compose exec -T app php artisan config:cache
 echo "starting php artisan migrate"
 docker-compose exec -T app php artisan migrate --force
 echo "starting php artisan passport:install"
-docker-compose exec -T app php artisan passport:install --force
+yes | docker-compose exec -T app php artisan passport:install --force
 echo "starting php artisan instance:actor"
 docker-compose exec -T app php artisan instance:actor
 # docker-compose restart app
